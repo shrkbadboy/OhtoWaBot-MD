e7f56cb67d7f5faa40f521b69c3c61e305a8eed2
shrkbadboy <yotsuba399@gmail.com>
2024-07-15 19:32:47 -0500

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,27 +1,5 @@
 let handler = async (m, { conn, command }) => {
-let gps = `*◜💙 GRUPOS OFICIALES 💙◞*
-
-🚩 ${packname} 
-${gp1}
-
-🚩 ${packname} 2
-${gp2}
-
- *◜🤍 COLABORACIONES 🤍◞*
-
-🍟 ${colab1}
-${gp3}
-
-🍟 ${colab2}
-${gp4}
-
-🍟 ${namecomu}
-${comunidad1}
-
-🍟 ${namecomu2}
-${comunidad2}
-
- *◜🌸 CANALES OFICIALES 🌸◞*
+let gps = `*◜🌸 CANALES OFICIALES 🌸◞*
 
 🍭 ${namechannel}
 ${channel}
@@ -31,11 +9,11 @@ ${channel2}`
 
 await m.react(emojis) 
 
-await conn.sendFile(m.chat, imagen3, 'luffy.jpg', gps, fkontak, null, rcanal)}
+await conn.sendFile(m.chat, imagen3, 'ai.jpg', gps, fkontak, null, rcanal)}
 
 handler.tags = ['main']
 handler.help = ['grupos']
-handler.command = /^grupos|gruposluffy|linkre|luffygp|gpluffy|gruposofc|gruposoficiales$/i
+handler.command = /^grupos|gruposai|linkre|aigp|gpai|gruposofc|gruposoficiales$/i
 handler.register = true
 
 export default handler
